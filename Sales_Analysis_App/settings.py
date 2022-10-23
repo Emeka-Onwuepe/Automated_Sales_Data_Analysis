14c4ee08b85a081eee0bf6508382272e82eb0207
EMEKA ONWUEPE <pascalemy2010@gmail.com>
2022-10-23 13:22:02 +0100

    feat: modelling

--- Sales_Analysis_App/settings.py
+++ Sales_Analysis_App/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
+    'users.apps.UsersConfig',
+    'data_sets.apps.DataSetsConfig',
 ]
 
 MIDDLEWARE = [
---
+++ data_sets/models.py
@@ -0,0 +1,37 @@
+from locale import currency
+from django.db import models
+
+from users.models import User_id
+
+# Create your models here.
+
+class Dataset(models.Model):
+    """Model definition for Dataset."""
+
+    # TODO: Define fields here
+    user_id  = models.ForeignKey(User_id, on_delete=models.CASCADE,related_name="dataset_user_id")
+    dataset_id = models.CharField("dataset_id",max_length = 150,blank=None)
+    dataset = models.FileField("dataset",blank=False)
+    report_title = models.FileField("report title",blank=False) 
+    currency_symbol = models.FileField("currency symbol",blank=False) 
+    zipfolder = models.FileField("zipfolder",blank=True)
+    
+    class Meta:
+        """Meta definition for Dataset."""
+
+        verbose_name = 'Dataset'
+        verbose_name_plural = 'Datasets'
+
+    def __str__(self):
+        """Unicode representation of Dataset."""
+        return f'{self.dataset_id} {self.dataset}'
+
+    def save(self):
+        """Save method for Dataset."""
+        pass
+
+    def get_absolute_url(self):
+        """Return absolute url for Dataset."""
+        return ('')
+
+    # TODO: Define custom methods here
