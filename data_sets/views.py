098dc902c3d7e669a513ec25277b1b5189069a9b
EMEKA ONWUEPE <pascalemy2010@gmail.com>
2022-10-28 02:08:22 +0100

    fix: read_dataset and classify

--- data_sets/views.py
+++ data_sets/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-from data_sets.models import Dataset
+import json
 import pandas as pd
+from data_sets.models import Dataset
 from source_code.statics import SUB_CLASSES
 from source_code.read_file import read_dataset
 
 # Create your views here.
 def ClassifyView(request,user_id,dataset_id):
-    df = read_dataset(Dataset,user_id,dataset_id,pd)
+    df,dataset = read_dataset(Dataset,user_id,dataset_id,pd)
     column = df.columns
     new_col = []
     for feature in column:
@@ -16,7 +17,9 @@ def ClassifyView(request,user_id,dataset_id):
         except AttributeError:
             feature = str(feature)
         feature = feature.replace("-","_").replace(" ","_")
-        new_col.append(feature)  
+        new_col.append(feature)
+    dataset.columns = json.dumps(new_col)
+    dataset.save()
     return render(request,'data_sets/classify.html',{"column":new_col,"sub_classes":SUB_CLASSES,
                                                     "user_id":user_id,"dataset_id":dataset_id})
 
@@ -38,7 +41,8 @@ def AnalysisView(request,user_id,dataset_id):
                 if value != "none":
                     mapper[value] = key
 
-        df = read_dataset(Dataset,user_id,dataset_id,pd)
-        # df = df[mapper.values()]
+        df,dataset = read_dataset(Dataset,user_id,dataset_id,pd)
+        df.columns = json.loads(dataset.columns)
+        df = df[mapper.values()]
 
     return render(request,"data_sets/dashboard.html",{"mapper":mapper,"df":df.head()})
--- source_code/read_file.py
+++ source_code/read_file.py
@@ -1,19 +1,20 @@
 
 def read_dataset(model,user_id,dataset_id,pd):
     df = None
+    dataset = None
 
     try:
 
-        data= model.objects.get(user_id__user_id = user_id,dataset_id = dataset_id)
-        _,file_extension = str(data.dataset).split(".")
+        dataset= model.objects.get(user_id__user_id = user_id, dataset_id= dataset_id)
+        _,file_extension = str(dataset.dataset).split(".")
         # load data
         if file_extension == 'xlsx':
-            df = pd.read_excel(data.dataset)
+            df = pd.read_excel(dataset.dataset)
         else:
             # remember to check for possibles errors
             #/t seperator and other non coma seperatorsS
-            df = pd.read_csv(data.dataset)
+            df = pd.read_csv(dataset.dataset)
     except model.DoesNotExist:
         pass
 
-    return df
+    return df,dataset
