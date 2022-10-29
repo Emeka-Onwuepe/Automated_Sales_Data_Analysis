ead1c9c4e71411dde497e2b4d1a06b1af1d842e9
EMEKA ONWUEPE <pascalemy2010@gmail.com>
2022-10-29 11:42:13 +0100

    fix: folder name

--- Sales_Analysis_App/settings.py
+++ Sales_Analysis_App/settings.py
@@ -138,13 +138,13 @@ AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID ")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = "casper-web-files"
 AWS_S3_FILE_OVERWRITE = False
-DEFAULT_FILE_STORAGE = 'SMBCLASSIC.aws_storage.MediaStorage'
+DEFAULT_FILE_STORAGE = 'Sales_Analysis_App.aws_storage.MediaStorage'
 AWS_DEFAULT_ACL = None
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-# STATICFILES_STORAGE = "SMBCLASSIC.aws_storage.StaticStorage"
+# STATICFILES_STORAGE = "Sales_Analysis_App.aws_storage.StaticStorage"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals()) 
